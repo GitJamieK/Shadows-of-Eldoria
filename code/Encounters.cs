ad6269298b2f694d2f9f2d0c04aa81bf697b502f
JamieK <koflerjamie@gmail.com>
2023-09-18 16:05:15 +0200

    added story in encounters

--- code/Encounters.cs
+++ code/Encounters.cs
@@ -194,25 +194,88 @@ namespace Game
         {
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.DarkMagenta;
-            Console.WriteLine("TEMP");
+            Console.WriteLine("In the days of yore, as the sun dipped below the horizon, the valiant knight by your side, clad in armor resplendent, turned his gaze towards the darkening forest. ");
+            Console.WriteLine("His visage spoke of ages spent in pursuit of the mystical and the arcane. ");
+            Console.WriteLine("In a hushed tone, he whispered of a thunderous clamor that had echoed through the ancient woods, ");
+            Console.WriteLine("a clamor that bore the promise of the third and final knight, fated to aid in the quest for the enigmatic crystal orbs. ");
             Console.WriteLine("");
             Console.ResetColor();
             Console.Write("Press any key to continue.\n>_");
             Tools.Loading();
-            //ADD STORY (3RD KNIGHT)
-            //ADD STORY (3RD KNIGHT)
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("");
+            Console.WriteLine("Without hesitation, you both embarked on a perilous journey towards the source of that clamor. ");
+            Console.WriteLine("There, amid gnarled trees and the eerie silence of twilight, lay the third knight, grievously wounded from a fierce and mighty battle. ");
+            Console.WriteLine("The second knight among you, renowned for swiftness of action, rushed to their side, bearing two vials of life-restoring elixir.");
+            Console.WriteLine("");
+            Console.ResetColor();
+            Console.Write("Press any key to continue.\n>_");
+            Tools.Loading();
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("");
+            Console.WriteLine("As the elixir coursed through the wounded knight's veins, strength returned to their battered form, and consciousness once more graced their eyes. ");
+            Console.WriteLine("With purpose, you and the second knight imparted the royal quest that had been entrusted to you by the noble queen herself, the perilous mission to locate the legendary crystal orbs, ");
+            Console.WriteLine("the only hope to vanquish the malevolent Malakar and safeguard the realm of Eldoria.");
+            Console.WriteLine("");
+            Console.ResetColor();
+            Console.Write("Press any key to continue.\n>_");
+            Tools.Loading();
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("");
+            Console.WriteLine("With newfound unity and a shared sense of destiny, you set forth, your footsteps echoing through the haunted woods, ");
+            Console.WriteLine("determined to find these elusive orbs and, in doing so, rewrite the fate of Eldoria.");
+            Console.WriteLine("");
+            Console.ResetColor();
+            Console.Write("Press any key to continue.\n>_");
+            Tools.Loading();
         }
         public static void SpecialEncounter3() //encounter: ORBS
         {
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.DarkMagenta;
-            Console.WriteLine("TEMP ORBS");
+            Console.WriteLine("In the midst of a relentless battle against the indomitable shadow warriors conjured forth by the malevolent sorcerer, Malakar, a peculiar revelation unfurled itself before your gallant trio. ");
+            Console.WriteLine("Yonder, within the weathered stone walls, an enigmatic crevice beckoned, casting forth a faint, otherworldly luminescence. ");
+            Console.WriteLine("You, being the astute soul, shared this extraordinary discovery with your two steadfast comrades. ");
+            Console.WriteLine("Together, with valor in your hearts, you ventured closer to the beckoning aperture.");
             Console.WriteLine("");
             Console.ResetColor();
             Console.Write("Press any key to continue.\n>_");
             Tools.Loading();
-            //ADD STORY (ORBS)
-            //ADD STORY (ORBS)
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("");
+            Console.WriteLine("One of your knightly companions, his eyes alight with astonishment, uttered a hushed conjecture that this might very well be the elusive sanctuary harboring the legendary crystal orbs.");
+            Console.WriteLine("Though their whereabouts remained shrouded in mystery, whispers of their existence had long pervaded the realm.");
+            Console.WriteLine("If these luminescent spheres were indeed the fabled orbs, then the murmurs held a kernel of truth.");
+            Console.WriteLine("");
+            Console.ResetColor();
+            Console.Write("Press any key to continue.\n>_");
+            Tools.Loading();
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("");
+            Console.WriteLine("The trio, with hardly an inch to spare, squeezed through the narrow passage, arriving in a chamber of modest proportions. ");
+            Console.WriteLine("Here, an imposing, circular stone portal obstructed the source of that ethereal azure radiance. ");
+            Console.WriteLine("One of your stalwart allies posited that, if the lore proved reliable, a concealed mechanism must be concealed within this chamber.");
+            Console.WriteLine("");
+            Console.ResetColor();
+            Console.Write("Press any key to continue.\n>_");
+            Tools.Loading();
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("");
+            Console.WriteLine("You, ever vigilant, espied a peculiar stone upon the wall, its visage ever so subtly distinct from the surrounding stones. ");
+            Console.WriteLine("Sharing your find with your comrades, they urged you to manipulate it. ");
+            Console.WriteLine("With a resolute press, the ponderous stone gateway glided aside, unveiling a mesmerizing sight - the resplendent, cerulean-glowing crystal orbs.");
+            Console.WriteLine("");
+            Console.ResetColor();
+            Console.Write("Press any key to continue.\n>_");
+            Tools.Loading();
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("");
+            Console.WriteLine("The awe that gripped your trio, however, yielded swiftly to the sobering reality of your quest. ");
+            Console.WriteLine("This realm teetered on the precipice of calamity, and the fate of the kingdom now hung heavy upon your shoulders. ");
+            Console.WriteLine("Without delay, each of you claimed one of the radiant orbs, resolved to confront the malevolent Malakar and consign his reign of madness to history's annals.");
+            Console.ResetColor();
+            Console.Write("Press any key to continue.\n>_");
+            Tools.Loading();
         }
         public static void SpecialEncounter4() //encounter: BOSS
         {
@@ -275,11 +338,14 @@ namespace Game
                             }
                             else
                             {
-                                if (Program.specialEncounter4Occurred)
+                                if (Program.RandomEncounterCount >= 20)
                                 {
-                                    SpecialEncounter4();
-                                    Program.specialEncounter4Occurred = true;
-                                    Program.SpecialEncounterCount++;
+                                    if (Program.specialEncounter4Occurred)
+                                    {
+                                        SpecialEncounter4();
+                                        Program.specialEncounter4Occurred = true;
+                                        Program.SpecialEncounterCount++;
+                                    }
                                 }
                             }
                         }
